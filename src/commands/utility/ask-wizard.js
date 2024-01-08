a888123b2fbb5837d066efc22820a130246e21aa
Milyn <milyng.dsilva@gmail.com>
2024-01-09 00:45:27 +0530

    adds gpt 3.5 turbo integration

--- src/commands/utility/ask-wizard.js
+++ src/commands/utility/ask-wizard.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
-const OpenAI = require('openai');
+const axios = require('axios');
 const logger = require('../../../logger/logger');
-const openai = new OpenAI({
-    apiKey: process.env.OPEN_AI_API_KEY
-});
-const model = "text-davinci-003";
+const apiKey = process.env.OPEN_AI_API_KEY;
+const API_ENDPOINT = process.env.OPEN_AI_API_URL;
+const OPEN_AI_MODEL = 'gpt-3.5-turbo';
+const allowdeUsers = ['milyndsilva', 'leanderrobin']
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -16,6 +16,11 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.deferReply();
+        const interactionUser = interaction.user.username;
+        if (!allowdeUsers.includes(interactionUser)) {
+            await interaction.editReply({ content: `Unfortunately, You don't have the necessary permissions to execute this command!` })
+            return;
+        }
         const clientMessage = interaction.options.getString('question') || null;
 
         let response = '';
@@ -23,16 +28,43 @@ module.exports = {
         if (clientMessage !== null) {
             const promptText = clientMessage;
 
-            const completion = await openai.completions.create({
-                model: model,
-                prompt: promptText,
-                max_tokens: 50,
-            });
-            response = completion.choices[0].text.trim().replace(/^\s+/, '');
-            logger.info(response);
+            try {
+                const axios = require('axios');
+                let data = JSON.stringify({
+                    "model": OPEN_AI_MODEL,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": promptText
+                        }
+                    ],
+                    "temperature": 0.7
+                });
+
+                let config = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: API_ENDPOINT,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${apiKey}`
+                    },
+                    data: data
+                };
+
+                axios.request(config)
+                    .then(async (result) => {
+                        response = result.data.choices[0].message.content;
+                        logger.info(response);
+                        await interaction.editReply(`***Question:*** ${clientMessage}\n***Response:*** ${response}`);
+                    })
+
+            } catch (error) {
+                logger.error(error);
+                await interaction.editReply(`***Error***: Something went wrong, check the logs for info.`);
+                return;
+            }
+
         }
-
-        await interaction.editReply(`***Question:*** ${clientMessage}\n***Response:*** ${response}`);
-
     },
 };
--- src/commands/utility/text-to-image.js
+++ src/commands/utility/text-to-image.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const logger = require('../../../logger/logger');
 const axios = require('axios');
-const provider = 'stabilityai';
+const provider = 'stabilityai'; //stabilityai , openai, deepai
+const allowdeUsers = ['milyndsilva','leanderrobin']
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,6 +14,11 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.deferReply();
+        const interactionUser = interaction.user.username;
+        if (!allowdeUsers.includes(interactionUser)) {
+            await interaction.editReply({ content: `Unfortunately, You don't have the necessary permissions to execute this command!` })
+            return;
+        }
         const clientMessage = interaction.options.getString('text') || null;
 
         let response = '';
@@ -41,7 +47,7 @@ module.exports = {
                 response = result.data[provider].items[0]["image_resource_url"]
                 const embed = new EmbedBuilder()
                     .setImage(response);
-                await interaction.editReply({ embeds: [embed] })
+                await interaction.editReply({ content: `***Prompt:*** ${clientMessage}`, embeds: [embed] })
             })
             .catch((error) => {
                 logger.error(error);
