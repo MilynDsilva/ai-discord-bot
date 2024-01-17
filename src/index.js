80f40d6cb1364ea75ca941e91251016ee31373a0
Milyn <milyng.dsilva@gmail.com>
2024-01-17 20:01:05 +0530

    fix: disables http server

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 const path = require('path');
 const fs = require('fs');
-const keepAlive = require('../src/server/keep-alive')
+//const keepAlive = require('../src/server/keep-alive')
 
 client.commands = new Collection();
 
